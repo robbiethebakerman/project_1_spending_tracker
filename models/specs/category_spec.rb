1e72d8865ff41417ded123154dec14601107cfb8
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-27 15:30:52 +0100

    Adds specs and writes category_spec, tidies up sql and seeds

---
+++ models/specs/category_spec.rb
@@ -0,0 +1,28 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../category')
+
+class  TestCategory < MiniTest::Test
+
+  def setup
+    options = {
+      'id' => 1,
+      'name' => 'Groceries',
+      'colour' => 'Blue'
+    }
+    @category = Category.new(options)
+  end
+
+  def test_category_has_id
+    assert_equal('1',@category.id)
+  end
+
+  def test_category_has_name
+    assert_equal('Groceries',@category.name)
+  end
+
+  def test_category_has_colour
+    assert_equal('Blue',@category.colour)
+  end
+
+end
---
+++ models/specs/transaction_spec.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('')
+
+class  Test < MiniTest::Test
+
+  def setup
+    options = {
+      'id' => 1,
+      '' =>
+    }
+    @thing = .new(options)
+  end
+
+  def test_
+    assert_equal('', )
+  end
+
+end
