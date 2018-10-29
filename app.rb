ad12a3e945e268e7f1b0defab51872883c17916a
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-29 10:16:15 +0000

    Adds Transactions Total Spent figure to budget bar

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require('sinatra/contrib/all')
 require_relative('controllers/categories_controller')
 require_relative('controllers/sellers_controller')
 require_relative('controllers/transactions_controller')
+require_relative('models/transaction')
 
 get '/' do
   erb(:index)
