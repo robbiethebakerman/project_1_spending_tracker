6cf7dba3a46f280792bf3bdd456ad76359ada46d
Robbie Baker <robbiethebakerman@googlemail.com>
2018-10-30 12:56:10 +0000

    Adds set budget page

--- controllers/budgets_controller.rb
+++ controllers/budgets_controller.rb
@@ -7,3 +7,9 @@ get "/budget/set/:type" do
   @budget = Budget.find_by_type(params[:type])
   erb(:"budgets/edit")
 end
+
+post "/budget/set/:id/:type" do
+  budget = Budget.new(params)
+  budget.update_amount()
+  redirect to "/"
+end
